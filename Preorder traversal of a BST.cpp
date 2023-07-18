70d3201d40eeb72dbae6bf55534380434d6ea4f5
Symon karim <symon.karim01@gmail.com>
2023-07-18 19:26:20 +0600

    Update Preorder traversal of a BST.cpp

--- Preorder traversal of a BST.cpp
+++ Preorder traversal of a BST.cpp
@@ -25,6 +25,8 @@
     };
 */
 
+// we can also solve this ques in O(N^2) 
+// O(nlogn) space O(1);
 BinaryTreeNode<int>*solve(int start, int end,vector<int>&preorder){
   if(start > end ) return NULL;
 
@@ -34,8 +36,21 @@ BinaryTreeNode<int>*solve(int start, int end,vector<int>&preorder){
   root->right = solve(mid+1,end,preorder);
   return root;
 }
+// Complexity O(N)
+BinaryTreeNode<int>*solveOptimized(int min, int max,vector<int> &pre,int &i ){
+   if(i>=pre.size()) return NULL;
+   if(pre[i]<min or pre[i]>max) return NULL;
+  
+    BinaryTreeNode<int>* root =new BinaryTreeNode<int>(pre[i++]);
+    root->left = solveOptimized(min, root->data, pre, i);
+    root->right = solveOptimized(root->data, max, pre, i);
+    return root;
+}
+
 BinaryTreeNode<int>* preorderToBST(vector<int> &preorder) {
-    sort(preorder.begin(), preorder.end());
-    int n = preorder.size();
-    return solve(0, n-1, preorder);
+    // sort(preorder.begin(), preorder.end());
+    // int n = preorder.size();
+    // return solve(0, n-1, preorder);
+    int min = INT_MIN, max = INT_MAX,i=0;
+    return solveOptimized( min,max,preorder, i);
 }
